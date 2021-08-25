40afcedab11d083dfa140cc72dc53f3e00afc952
kim-hyunjin <hyunjin1612@gmail.com>
2021-08-25 21:49:00 +0900

    proxy 설정, api로 board 가져오기

--- src/store/BoardStore.ts
+++ src/store/BoardStore.ts
@@ -21,29 +21,10 @@ class BoardList {
   }
 
   fetchBoards() {
-    // // fetch data from backend
-    // axios.get("http://localhost:8080/boards").then((res) => {
-    //   this.setBoards(res.data);
-    // });
-    this.boards = [
-      {
-        id: 1,
-        title: "여 아이돌 이상형 월드컵",
-        writer: "김씨",
-        description: "2020년대 여 아이돌 가수 이상형 월드컵",
-      },
-      {
-        id: 2,
-        title: "남 아이돌 이상형 월드컵",
-        writer: "이씨",
-      },
-      {
-        id: 2,
-        title: "해외 팝싱어 이상형 월드컵",
-        writer: "박씨",
-        description: "2010년 이후 해외 팝싱어 이상형 월드컵",
-      },
-    ];
+    // fetch data from backend
+    axios.get("/board").then((res) => {
+      this.setBoards(res.data);
+    });
   }
 
   setSelectedBoard(board: Board) {
